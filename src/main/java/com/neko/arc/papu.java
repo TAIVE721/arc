67b2ef256aaabaa834bb76bc24da081cadb8559e
TAIVE <davidcorachuctaya@gmail.com>
2025-08-27 12:21:36 -0500

    a

---
+++ src/main/java/com/neko/arc/papu.java
@@ -0,0 +1,19 @@
+package com.neko.arc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class papu {
+
+    private Number getpapu() {
+        return 34;
+    }
+
+    @RequestMapping("/hola")
+    public Number papu() {
+        Number index = getpapu();
+        return index;
+    }
+
+}
